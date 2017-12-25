0a822fe5421399f848340f3132807979109bb718
prerit <preritmisra@hotmail.com>
2017-12-25 10:47:08 +0530

    trying out merge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,9 @@
 Rails.application.configure do
   #for devises use on local host ONLY!!In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -60,7 +62,19 @@ Rails.application.configure do
   # To send emails to check if user email valid or not(self comment)
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
     
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207152444) do
+ActiveRecord::Schema.define(version: 20171220112953) do
 
   create_table "links", force: :cascade do |t|
     t.datetime "created_at", null: false
